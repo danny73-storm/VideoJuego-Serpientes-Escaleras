eebd3d78c9d92cf80d09fd1e6310f00d59b5a868
Daniel Ixcaquic <danielalexanderib73@gmail.com>
2021-06-25 17:45:07 -0600

    commit 1

---
+++ SerpientesEscaleras_ProyectoFinal_IPC1_VaquerasJunio/src/main/java/interfaces/Principal.java
@@ -0,0 +1,15 @@
+
+package interfaces;
+
+/**
+ *
+ * @author Danny
+ */
+public class Principal {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
