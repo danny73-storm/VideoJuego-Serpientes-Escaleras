27107e4c2d7a3188728477a42fc5649a074e3e29
Daniel Ixcaquic <danielalexanderib73@gmail.com>
2021-06-28 20:34:58 -0600

    commit 5

--- SerpientesEscaleras_ProyectoFinal_IPC1_VaquerasJunio/src/main/java/juego/Escalera.java
+++ SerpientesEscaleras_ProyectoFinal_IPC1_VaquerasJunio/src/main/java/juego/Escalera.java
@@ -9,6 +9,21 @@ package juego;
  *
  * @author Danny
  */
-public class Escalera {
+public class Escalera extends Fichas {
+    private int irX, irY;
     
+    public Escalera(int posicionX, int posicionY, int irX, int irY) {
+        this.posicionX = posicionX;
+        this.posicionY = posicionY;
+        this.irX = irX;
+        this.irY = irY;
+    }
+    
+    public int getIrX() {
+        return irX;
+    }
+
+    public int getIrY() {
+        return irY;
+    }
 }
