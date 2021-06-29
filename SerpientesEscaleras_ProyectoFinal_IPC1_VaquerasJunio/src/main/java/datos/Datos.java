32fc6892f620a0304a180eb59a24f3397f37715a
Daniel Ixcaquic <danielalexanderib73@gmail.com>
2021-06-29 12:29:13 -0600

    commit 10

---
+++ SerpientesEscaleras_ProyectoFinal_IPC1_VaquerasJunio/src/main/java/datos/Datos.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datos;
+
+/**
+ *
+ * @author Danny
+ */
+public abstract class Datos {
+    protected String archivo;
+    
+    public Datos(String archivo) {
+        this.archivo = archivo;
+    }
+    
+    /**
+     * Confirma si el archivo existe.
+     * 
+     * @return boolean
+     */
+    abstract public boolean existeArchivo();
+
+    /**
+     * Obtiene el nombre del archivo
+     * @return 
+     */
+    public String getArchivo() {
+        return archivo;
+    }
+    
+}
