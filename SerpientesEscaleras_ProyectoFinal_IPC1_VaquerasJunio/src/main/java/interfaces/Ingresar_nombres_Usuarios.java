76f1b59cede39a27fbf8c9bd5017397f220b6210
Daniel Ixcaquic <danielalexanderib73@gmail.com>
2021-06-29 10:05:51 -0600

    commit 7

--- SerpientesEscaleras_ProyectoFinal_IPC1_VaquerasJunio/src/main/java/interfaces/Ingresar_nombres_Usuarios.java
+++ SerpientesEscaleras_ProyectoFinal_IPC1_VaquerasJunio/src/main/java/interfaces/Ingresar_nombres_Usuarios.java
@@ -31,7 +31,6 @@ public class Ingresar_nombres_Usuarios extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jCheckBoxMenuItem1 = new javax.swing.JCheckBoxMenuItem();
         jPanel2 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
@@ -62,13 +61,6 @@ public class Ingresar_nombres_Usuarios extends javax.swing.JFrame {
         jCheckBoxMenuItem1.setText("jCheckBoxMenuItem1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jButton1.setText("Cerrar");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
 
         jLabel1.setText("Ingrese el nombre de los jugadores");
 
@@ -107,8 +99,7 @@ public class Ingresar_nombres_Usuarios extends javax.swing.JFrame {
                         .addGap(27, 27, 27)
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(jPanel2Layout.createSequentialGroup()
-                                .addComponent(jButton1)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 253, Short.MAX_VALUE)
+                                .addGap(0, 325, Short.MAX_VALUE)
                                 .addComponent(jButton5))
                             .addComponent(jLabel1)))
                     .addGroup(jPanel2Layout.createSequentialGroup()
@@ -160,13 +151,8 @@ public class Ingresar_nombres_Usuarios extends javax.swing.JFrame {
                     .addComponent(jLabel7)
                     .addComponent(jTextField6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 13, Short.MAX_VALUE)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                        .addComponent(jButton1)
-                        .addContainerGap())
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                        .addComponent(jButton5)
-                        .addGap(18, 18, 18))))
+                .addComponent(jButton5)
+                .addGap(18, 18, 18))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -188,10 +174,6 @@ public class Ingresar_nombres_Usuarios extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        
-    }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
         MenuPrincipal menu = new MenuPrincipal();
@@ -239,7 +221,6 @@ public class Ingresar_nombres_Usuarios extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton5;
     private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem1;
     private javax.swing.JLabel jLabel1;
--- SerpientesEscaleras_ProyectoFinal_IPC1_VaquerasJunio/src/main/java/interfaces/MenuPrincipal.java
+++ SerpientesEscaleras_ProyectoFinal_IPC1_VaquerasJunio/src/main/java/interfaces/MenuPrincipal.java
@@ -120,11 +120,13 @@ public class MenuPrincipal extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        EstadisticasReportes reportes = new EstadisticasReportes();
+        reportes.setVisible(true);
+        this.setVisible(false);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        this.dispose();
     }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
